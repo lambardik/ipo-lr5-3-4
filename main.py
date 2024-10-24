20ae87a5655518bfdc0e4ce01067815c2d44fd0c
даниэль <dannilboyar@gmail.com>
2024-10-24 10:32:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def reverse_words(text):
-
-  words = text.split()
-  reversed_words = [word[::-1] for word in words]
-  return ' '.join(reversed_words)
-
 text = input("Введите строку: ")
-reversed_text = reverse_words(text)
+reversed_words = [word[::-1] for word in text.split()]
+reversed_text = ' '.join(reversed_words)
 print(f"Развернутая строка: {reversed_text}")
